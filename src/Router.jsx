1b360003cab6ec0f93feb9bfe9c3fc0f5105cfc5
raph2ll <raphael.ph2ll@gmail.com>
2022-09-01 15:41:06 -0300

    refactor:  add router and refactor app schema

---
+++ src/Router.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default Router
