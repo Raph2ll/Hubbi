3f98c58a27cfaa082b4f810ccda58191ccd3aae5
raph2ll <raphael.ph2ll@gmail.com>
2022-09-01 23:23:15 -0300

    feat(main): global css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+import './styles/global.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
