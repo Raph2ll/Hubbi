0f9a8910c64daae732b909b488e48afc73831ac1
raph2ll <raphael.ph2ll@gmail.com>
2022-09-02 19:05:42 -0300

    feat(StarshipsCard): condicional component

--- src/Components/StarshipsCard.jsx
+++ src/Components/StarshipsCard.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../Context/AuthContext/useAuth";
 
 function StarshipsCard(element) {
   const { data } = element
+  const auth = useAuth()
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center m-3 w-56">
       <p className="text-gray-800">{data.name}</p>
@@ -9,13 +12,12 @@ function StarshipsCard(element) {
         <p className="text-gray-800">Model: {data.model}</p>
       </div>
       <div className="flex flex-row space-x-1">
-        <Link to={`/starships/${data.url.substring(data.url.indexOf("/", 29), data.url.length - 1).replace("/", "")}`}>
+        {auth.email && (<Link to={`/starships/${data.url.substring(data.url.indexOf("/", 29), data.url.length - 1).replace("/", "")}`}>
           <button
             className="w-16 px-1 py-1 text-xs tracking-wide text-white transition-colors duration-200 transform bg-blue-100 hover:bg-blue-200 rounded-md ">
             Details
           </button>
-        </Link>
-
+        </Link>)}
       </div>
     </div>
   );
