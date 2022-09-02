addadc1f5cadcbabdd5b0eee58bf4d3da4436971
raph2ll <raphael.ph2ll@gmail.com>
2022-09-01 22:19:26 -0300

    feat(Router): add new paths

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
+import People from './pages/People';
+import Starships from './pages/Starships';
+import StarshipsDetails from './pages/StarshipsDetails';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/people" element={<People />} />
+      <Route path="/starships" element={<Starships />} />
+      <Route path="/starships/:id" element={<StarshipsDetails />} />
     </Routes>
   )
 }
