b397652cde327e050de4b7d8812150fca89c44a6
raph2ll <raphael.ph2ll@gmail.com>
2022-09-02 11:59:40 -0300

    refactor(StarshipsDetails): fix details

--- src/pages/StarshipsDetails.jsx
+++ src/pages/StarshipsDetails.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"
+import Nav from "../Components/Nav";
 
 function StarshipsDetails() {
   const [details, setDetails] = useState([]);
@@ -18,19 +18,18 @@ function StarshipsDetails() {
   };
 
   return (
-    <div>
-      <h1>Name: {details.name}</h1>
-      <h1>Mode: {details.model}</h1>
-      <h1>Crew: {details.crew}</h1>
-      <h1>Passengers: {details.passengers}</h1>
-      <h1>StarshipClass: {details.starship_class}</h1>
-
-      <Link to={`/`}>
-        <button
-          type="button"
-        >Voltar
-        </button>
-      </Link>
+    <div className="flex flex-col min-h-screen items-center">
+      <Nav />
+      <div className="flex justify-center items-center flex-row flex-wrap bg-white p-6 rounded-lg shadow-md m-3 w-56">
+        <h1p className="text-gray-800">{details.name}</h1p>
+        <div className="flex flex-col my-6">
+          <p className="text-gray-800">Name: {details.name}</p>
+          <p className="text-gray-800">Model: {details.model}</p>
+          <p className="text-gray-800">Crew: {details.crew}</p>
+          <p className="text-gray-800">Passengers: {details.passengers}</p>
+          <p className="text-gray-800">StarshipClass: {details.starship_class}</p>
+        </div>
+      </div>
     </div >
   )
 }
