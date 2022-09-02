5cca5844c683c1941e8e80750a23fe046a4f21ed
raph2ll <raphael.ph2ll@gmail.com>
2022-09-02 18:44:08 -0300

    feat(AuthContext): create Login

---
+++ src/Context/AuthContext/util.js
@@ -0,0 +1,26 @@
+import { Api } from "../../services/api"
+
+export function setUserLocalStorage (user) {
+  localStorage.setItem("u", JSON.stringify(user));
+};
+
+export function getUserLocalStorage () {
+  const json = localStorage.getItem("u");
+
+  if (!json) { 
+    return null;
+  }
+
+  const user = JSON.parse(json);
+  return user ?? null;
+}
+
+export async function LoginRequest (email, password) {
+  try {
+    const request = await Api.post("login", { email, password })
+
+    return request.data;
+  } catch (error) {
+    return null;
+  };
+};
