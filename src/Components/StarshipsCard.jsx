58573cae4ce47d3333619fc159868250b0c883cb
raph2ll <raphael.ph2ll@gmail.com>
2022-09-02 02:14:08 -0300

    fix(StarshipsCard): details

--- src/Components/StarshipsCard.jsx
+++ src/Components/StarshipsCard.jsx
@@ -6,10 +6,7 @@ function StarshipsCard(element) {
     <div className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center m-3 w-56">
       <p className="text-gray-800">{data.name}</p>
       <div className="flex flex-col my-6">
-        <p className="text-gray-800">Height: {data.height}</p>
-        <p className="text-gray-800">Hair color: {data.hair_color}</p>
-        <p className="text-gray-800">Skin color : {data.skin_color}</p>
-        <p className="text-gray-800">Eye color : {data.eye_color}</p>
+        <p className="text-gray-800">Model: {data.model}</p>
       </div>
       <div className="flex flex-row space-x-1">
         <Link to={`/starships/${data.url.substring(data.url.indexOf("/", 29), data.url.length - 1).replace("/", "")}`}>
