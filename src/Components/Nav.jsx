fa5359e3146e5f5d2d4070a378472a97d3f27bdb
raph2ll <raphael.ph2ll@gmail.com>
2022-09-02 01:48:56 -0300

    feat(Nav): create a new component

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <nav className="w-full px-32 h-12 flex items-center justify-between bg-white border-b border-gray-200">
+      <header className="flex items-center">
+        <div className="hidden md:block">
+          <div className="ml-10 mr-52 flex place-items-baseline space-x-4">
+            <Link to={"/"}>
+              <a href="google.com" className="text-sm text-gray-800 p-6">Home</a>
+            </Link>
+          </div>
+        </div>
+        {/* Logo
+        <div className="flex-shrink-0">
+        </div>*/}
+      </header>
+    </nav>
+  );
+}
+
+export default Nav;
